a69d6cc752da61525f6dee3b255b67c08d29a8d1
Amy Boyd <amy@amyboyd.co.uk>
2020-02-06 04:51:29 +0000

    Add failing test for 'Numeric Annotation Glyphs aren't correctly removed'.
    Issue: https://github.com/amyboyd/pgn-parser/issues/1

--- test/AmyBoyd/PgnParser/GameTest.php
+++ test/AmyBoyd/PgnParser/GameTest.php
@@ -25,4 +25,20 @@ class GameTest extends PHPUnit_Framework_TestCase
       $this->assertInternalType('array', json_decode($json, true));
       $this->assertJsonStringEqualsJsonFile(__DIR__ . '/expected-toJSON-1.json', $json);
     }
+
+    public function testGetMoves()
+    {
+      $file = 'test/data/200open.pgn';
+      $parser = new PgnParser($file);
+      $game = $parser->getGame(0);
+
+      $this->assertEquals(
+        'e4 e5 Nf3 d6 d4 f5 dxe5 fxe4 Ng5 d5 e6 Nh6 Nc3 c6 Ngxe4 dxe4 Qh5+ g6 Qe5 Rg8 Bxh6 Bxh6 Rd1 Qg5 Qc7 Bxe6 Qxb7 e3 f3 Qe7 Qxa8 Kf7 Ne4 Bf4 Be2 Kg7 O-O Qc7 Nc5 Bxh2+ Kh1 Bc8 Rd4 Bg3 Re4 Kh8 Rd1 Qg7 Rh4 Bxh4 Qxb8 Ba6 Qh2 Bxe2 Rd7 Qh6 Ne4 Bc4 Nf6 e2 Re7 Qc1+ Qg1 Qxg1+ Kxg1 e1=Q+ Rxe1 Bxe1',
+        $game->getMoves()
+      );
+      $this->assertEquals(
+        68,
+        $game->getMovesCount()
+      );
+    }
 }
